c43245eda67b8be3bf3ca27424559d7d5cead8e2
Mikhail A <mg@abramishvili.net>
2021-10-14 21:14:58 +0500

    .

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Номерус</title>
         <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet"/>
     </head>
