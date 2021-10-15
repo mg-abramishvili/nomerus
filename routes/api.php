b232e9004e5069224a0a32d3b3f95d6f8a6b90b0
Mikhail A <mg@abramishvili.net>
2021-10-15 15:56:14 +0500

    .

--- routes/api.php
+++ routes/api.php
@@ -35,46 +35,46 @@ Route::post('/login', 'App\Http\Controllers\Admin\AuthController@login');
 Route::post('/me', 'App\Http\Controllers\Admin\AuthController@me');
 
 Route::get('admin/orders','App\Http\Controllers\Admin\OrderController@orders_index')->middleware('auth:sanctum');
-Route::get('admin/order-items','App\Http\Controllers\Admin\OrderItemController@orderItems_index');
+Route::get('admin/order-items','App\Http\Controllers\Admin\OrderItemController@orderItems_index')->middleware('auth:sanctum');
 
-Route::get('admin/certificates','App\Http\Controllers\Admin\CertificateController@index');
-Route::post('admin/certificates','App\Http\Controllers\Admin\CertificateController@store');
-Route::get('admin/certificate/{id}','App\Http\Controllers\Admin\CertificateController@cert_item');
-Route::post('admin/certificate/{id}','App\Http\Controllers\Admin\CertificateController@cert_update');
+Route::get('admin/certificates','App\Http\Controllers\Admin\CertificateController@index')->middleware('auth:sanctum');
+Route::post('admin/certificates','App\Http\Controllers\Admin\CertificateController@store')->middleware('auth:sanctum');
+Route::get('admin/certificate/{id}','App\Http\Controllers\Admin\CertificateController@cert_item')->middleware('auth:sanctum');
+Route::post('admin/certificate/{id}','App\Http\Controllers\Admin\CertificateController@cert_update')->middleware('auth:sanctum');
 Route::post('admin/certificates/add_image_upload','App\Http\Controllers\Admin\CertificateController@add_image_store')->middleware('auth:sanctum');
 
-Route::get('admin/gallery','App\Http\Controllers\Admin\GalleryController@index');
-Route::post('admin/gallery','App\Http\Controllers\Admin\GalleryController@store');
-Route::post('admin/gallery/add_image_upload','App\Http\Controllers\Admin\GalleryController@temp_gal_image_store');
-Route::get('admin/gallery-del/{id}','App\Http\Controllers\Admin\GalleryController@delete_item');
+Route::get('admin/gallery','App\Http\Controllers\Admin\GalleryController@index')->middleware('auth:sanctum');
+Route::post('admin/gallery','App\Http\Controllers\Admin\GalleryController@store')->middleware('auth:sanctum');
+Route::post('admin/gallery/add_image_upload','App\Http\Controllers\Admin\GalleryController@temp_gal_image_store')->middleware('auth:sanctum');
+Route::get('admin/gallery-del/{id}','App\Http\Controllers\Admin\GalleryController@delete_item')->middleware('auth:sanctum');
 
-Route::get('admin/cities','App\Http\Controllers\Admin\CityController@index');
-Route::get('admin/addresses','App\Http\Controllers\Admin\AddressController@index');
-Route::post('admin/addresses','App\Http\Controllers\Admin\AddressController@store');
-Route::get('admin/address/{id}','App\Http\Controllers\Admin\AddressController@address_item');
-Route::post('admin/address/{id}','App\Http\Controllers\Admin\AddressController@address_update');
+Route::get('admin/cities','App\Http\Controllers\Admin\CityController@index')->middleware('auth:sanctum');
+Route::get('admin/addresses','App\Http\Controllers\Admin\AddressController@index')->middleware('auth:sanctum');
+Route::post('admin/addresses','App\Http\Controllers\Admin\AddressController@store')->middleware('auth:sanctum');
+Route::get('admin/address/{id}','App\Http\Controllers\Admin\AddressController@address_item')->middleware('auth:sanctum');
+Route::post('admin/address/{id}','App\Http\Controllers\Admin\AddressController@address_update')->middleware('auth:sanctum');
 
-Route::get('admin/types/{city}','App\Http\Controllers\Admin\TypeController@index');
-Route::get('admin/type/{type_id}/{city_id}','App\Http\Controllers\Admin\TypeController@type_item');
-Route::put('admin/type/{type_id}/{city_id}','App\Http\Controllers\Admin\TypeController@type_item_update');
+Route::get('admin/types/{city}','App\Http\Controllers\Admin\TypeController@index')->middleware('auth:sanctum');
+Route::get('admin/type/{type_id}/{city_id}','App\Http\Controllers\Admin\TypeController@type_item')->middleware('auth:sanctum');
+Route::put('admin/type/{type_id}/{city_id}','App\Http\Controllers\Admin\TypeController@type_item_update')->middleware('auth:sanctum');
 
-Route::get('admin/services','App\Http\Controllers\Admin\ServiceController@index');
-Route::get('admin/service/{id}','App\Http\Controllers\Admin\ServiceController@service_item');
-Route::post('admin/service/{id}','App\Http\Controllers\Admin\ServiceController@service_update');
-Route::post('admin/services/add_image_upload','App\Http\Controllers\Admin\ServiceController@add_image_store');
+Route::get('admin/services','App\Http\Controllers\Admin\ServiceController@index')->middleware('auth:sanctum');
+Route::get('admin/service/{id}','App\Http\Controllers\Admin\ServiceController@service_item')->middleware('auth:sanctum');
+Route::post('admin/service/{id}','App\Http\Controllers\Admin\ServiceController@service_update')->middleware('auth:sanctum');
+Route::post('admin/services/add_image_upload','App\Http\Controllers\Admin\ServiceController@add_image_store')->middleware('auth:sanctum');
 
-Route::get('admin/transports','App\Http\Controllers\Admin\TransportController@transports_index');
+Route::get('admin/transports','App\Http\Controllers\Admin\TransportController@transports_index')->middleware('auth:sanctum');
 
-Route::get('admin/partners','App\Http\Controllers\Admin\PartnerController@index');
-Route::post('admin/partners','App\Http\Controllers\Admin\PartnerController@store');
-Route::get('admin/partner/{id}','App\Http\Controllers\Admin\PartnerController@partner_item');
-Route::post('admin/partner/{id}','App\Http\Controllers\Admin\PartnerController@partner_update');
-Route::post('admin/partners/add_image_upload','App\Http\Controllers\Admin\PartnerController@add_image_store');
+Route::get('admin/partners','App\Http\Controllers\Admin\PartnerController@index')->middleware('auth:sanctum');
+Route::post('admin/partners','App\Http\Controllers\Admin\PartnerController@store')->middleware('auth:sanctum');
+Route::get('admin/partner/{id}','App\Http\Controllers\Admin\PartnerController@partner_item')->middleware('auth:sanctum');
+Route::post('admin/partner/{id}','App\Http\Controllers\Admin\PartnerController@partner_update')->middleware('auth:sanctum');
+Route::post('admin/partners/add_image_upload','App\Http\Controllers\Admin\PartnerController@add_image_store')->middleware('auth:sanctum');
 
-Route::get('admin/leads','App\Http\Controllers\Admin\LeadController@index');
+Route::get('admin/leads','App\Http\Controllers\Admin\LeadController@index')->middleware('auth:sanctum');
 
-Route::get('admin/text','App\Http\Controllers\Admin\TextController@index');
-Route::post('admin/text','App\Http\Controllers\Admin\TextController@update');
+Route::get('admin/text','App\Http\Controllers\Admin\TextController@index')->middleware('auth:sanctum');
+Route::post('admin/text','App\Http\Controllers\Admin\TextController@update')->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
