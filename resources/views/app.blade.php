7412dfc9e4fb4aa79cc767c43800487467da5b44
Mikhail A <mg@abramishvili.net>
2021-11-09 12:48:39 +0500

    .

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -35,8 +35,8 @@
             (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
             m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
             (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-            ym(86171157, "init", {
+        
+            ym(86309721, "init", {
                 defer: true,
                 clickmap:true,
                 trackLinks:true,
@@ -44,7 +44,7 @@
                 webvisor:true
             });
         </script>
-        <noscript><div><img src="https://mc.yandex.ru/watch/86171157" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <noscript><div><img src="https://mc.yandex.ru/watch/86309721" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
         <!-- /Yandex.Metrika counter -->
     </body>
 </html>
