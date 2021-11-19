531f1dfbdeb0a07cd3de0f0c54dbcdffc9832565
Mikhail A <mg@abramishvili.net>
2021-11-19 12:35:58 +0500

    .

--- resources/js/components/front/FrontLayout.vue
+++ resources/js/components/front/FrontLayout.vue
@@ -272,17 +272,6 @@
             .then(response => (
                 this.text = response.data
             ));
-
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-            ym(86309721, 'init', {
-                defer: true,
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true
-            })
         },
         methods: {
             goToPage(route_name) {
